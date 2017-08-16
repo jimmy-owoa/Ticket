ee2e53d85eff9abbaeb5d56ca6a1eec01284dacb
josnunezg <josnunezg@gmail.com>
2017-08-16 17:13:03 -0300

    asignacion de usuario

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,13 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
   enum status: [:unassigned, :assigned, :finished]
+
+  def assigned_user
+    User.find_by_id(user_assigned)
+  end
+
+  def assigned_user_name
+    return ""  unless user_assigned
+    assigned_user.name
+  end
 end
